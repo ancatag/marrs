89f119dad22ac69a21ca82d0c669be2a99358697
Abhilash P B <abhilashbaby95@gmail.com>
2018-04-10 14:23:09 +0530

    quit session, but some space is visible

--- assets/js/dictation.js
+++ assets/js/dictation.js
@@ -34,16 +34,20 @@ for (var i=0; i<questsPractDict.length; i++){
 $('.questsIndDiv0').css("display", "block");
 
 $('#dictPractQuespap').append("\
+<div class='button_class'>\
     <div class='row'>\
         <div class='col-sm-10'>\
         <a id='quitBtn' class='btn btn-danger quitAndSubmitBtn' onclick='quitAndSubmit(event)'>Quit Exam</a>\
+        <a id='goHome_Btn' class='btn btn-danger quitAndSubmitBtn' style='display:none'>Quit Exam</a>\
         <a class='btn btn-info' id='nxtBtn' onclick='displayNext()'>Next</a>\
         <button id='practiceDictResult_Btn' type='submit' class='btn btn-primary quitAndSubmitBtn' onclick='quitAndSubmit(event)' style='display:none'>Submit Answer</button>\
         <a id='detailReport_Btn' class='btn btn-warning' onclick='openReportCard();' style='display:none'>Detail Report</a>\
         </div>\
     </div>\
+    </div>\
       "); 
-
+      
+      $('#quitBtn').hide();
       function timeOut() {
         cdreset(questsPractDict.timeLimit);//timer reset
         countdown();//timer start
@@ -61,7 +65,7 @@ $('#dictPractQuespap').append("\
     }
              $('.questsIndDiv:visible:last').next().css("display", "block");
              $('.questsIndDiv:visible:last').prev().hide();
-            
+             $('#quitBtn').show();
             //  $('.questsIndDiv:visible:last').prev().show();
             var lastID =  $('.questsIndDiv:visible:last input').attr('id');
         // console.log($('.questsIndDiv:nth-last-child(2)'));
@@ -120,17 +124,43 @@ $('#dictPractQuespap').append("\
                     $("#quitBtn").trigger('click');
                     // $('#quitBtn').hide();
                     $('#nxtBtn').hide();
+                    $('#quitBtn').hide();
+                    $('#goHome_Btn').show();
                     cdpause();
                     clearInterval(timer);
                    
-                    $('.questsIndDiv:visible:last').css("display", "block").show();
-                    $('.questsIndDiv:visible:last').next().hide();
+                 
                 }
+        //          $('.questsIndDiv').on('click', '#quitBtn', function(event) {
+        //     event.preventDefault();
+        //     //$('.questsIndDiv:visible:last').show(60000).delay(60000);
+        //     $('.questsIndDiv:visible:last').remove();
+        //     $("#quitBtn").show();
+        //     $("#detailReport_Btn").show();
+        // //     $('.questsIndDiv:visible:last').show();
+        // });
+              
+               
             // clearInterval(timer);
+           
             
-        });    
+        });  
+      
+       
         }
-
+        $("#goHome_Btn").click(function(){ 
+    
+            if (confirm("You have no answered anything, do you want to redirect it to the home page?")) {
+                window.location.href = path+'stu'; 
+            }
+        });
+        // $("#qui
+        //   $(".questsIndDiv").fadeOut("normal", function() {
+        //                 // $('.questsIndDiv:visible:last').show();
+        //                 $('.questsIndDiv:visible:last').remove(60000);
+        //                 $('.questsIndDiv:visible:last').next().show();
+        //             });
+   
 // for (var i=0; i<questsPractDict.length; i++){
 //     $('#identPractQuespap').append("\
 //         <div class='questsIndDiv"+i+ " questsIndDiv media text-muted pt-3 newsfeeds-div container ' style='display:none'>\
@@ -202,7 +232,7 @@ function openReportCard(){
                         }else if(rsp.data.resultData[key] == '------'){
                             sk++;
                             var ht = "#"+key;
-                            $(ht).siblings('span').append("<i class='fa fa-ban'></i>");
+                            $(ht).siblings('span').html("<i class='fa fa-ban'></i>");
                             $("#detailReportModal_Body table").html("\<table>\
                     <tr><td>Total No.Of.Questions:</td><td>"+(sum+wr+sk)+"</td></tr>\
                     <tr><td>No.Of.Correct Answer:</td><td>"+sum+"</td></tr>\
@@ -215,7 +245,7 @@ function openReportCard(){
                             wr++;
                             //alert('Wrong');
                             var ht = "#"+key;
-                            $(ht).siblings('span').append("<i class='fa fa-close'></i>");
+                            $(ht).siblings('span').html("<i class='fa fa-close'></i>");
                             $("#detailReportModal_Body table").html("\<table>\
                     <tr><td>Total No.Of.Questions:</td><td>"+(sum+wr+sk)+"</td></tr>\
                     <tr><td>No.Of.Correct Answer:</td><td>"+sum+"</td></tr>\
@@ -258,3 +288,13 @@ function openReportCard(){
             }
         });
     }
+    $("#quitBtn").click(function(){ 
+        // if ($(".questsIndDiv:visible:last").length !=0){
+         
+        console.log($(".questsIndDiv:visible:last").nextAll());
+            
+           $(".questsIndDiv:visible:last").nextAll().css("visibility", "hidden");
+           $("#dictPractQuespap .button_class").css('visibility','visible');
+           console.log($("#dictPractQuespap .button_class"));
+        // }
+        });
